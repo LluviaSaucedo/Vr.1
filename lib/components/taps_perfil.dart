8f9ce2548388d0282c33731407bb6ecb5e2b1190
Lluvia Saucedo <lluvia.saucedo@mercadito.io>
2020-02-05 17:56:07 -0500

    Tab Bar listo en Perfil

--- lib/components/taps_perfil.dart
+++ lib/components/taps_perfil.dart
@@ -1,65 +1,21 @@
 import 'package:flutter/material.dart';
 
-import '../screens/spots.dart';
-import '../screens/perfil.dart';
-
 class Taps extends StatefulWidget {
   State<StatefulWidget> createState() => _TapsState();
 }
 
 class _TapsState extends State<Taps> {
-  int currentTab = 0;
-  final List<Widget> screens = [
-    Spots(),
-    Perfil(),
-  ];
-  final PageStorageBucket bucket = PageStorageBucket();
-  Widget currentScreen = Spots();
-
   @override
   Widget build(BuildContext context) {
-    return Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Row(
-            children: <Widget>[
-              FlatButton(
-                padding: EdgeInsets.only(left:60.00, right:60,),
-                onPressed: null,
-                child: Text("Mis Spot's",
-                    style: TextStyle(
-                      color: Colors.red[200],
-                    )),
-                textColor: Colors.black,
-                
-                shape: RoundedRectangleBorder(
-                    side: BorderSide(
-                        color: Colors.red[200],
-                        width: 1,
-                        style: BorderStyle.solid),
-                    borderRadius: BorderRadius.circular(10)),
-              )
-            ],
-          ),
-          Row(
-            children: <Widget>[
-              FlatButton(
-                padding: EdgeInsets.only(left:50, right:50,),
-                onPressed: null,
-                child: Text("Peculiaridad",
-                    style: TextStyle(
-                      color: Colors.red[200],
-                    )),
-                textColor: Colors.white,
-                shape: RoundedRectangleBorder(
-                    side: BorderSide(
-                        color: Colors.red[200],
-                        width: 1,
-                        style: BorderStyle.solid),
-                    borderRadius: BorderRadius.circular(10)),
-              )
-            ],
-          )
-        ]);
+    return DefaultTabController(
+      length: 2,
+      child: TabBar(
+        tabs: [
+          Tab(text: "Mis Spot's",icon: Icon(Icons.art_track)), 
+          Tab(text: 'Insignias',icon: Icon(Icons.star_border)),
+        ],
+        indicatorColor: Colors.red[200]
+      ),
+    );
   }
 }
